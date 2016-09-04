f8a9d2969824c11aab6036ce1035d66de29640bd
Devendra Meena <idvndr@gmail.com>
2016-09-04 10:49:18 +0530

    .js

---
+++ ads.js
@@ -0,0 +1,32 @@
+<div id='quickfever-content' style='text-align: center;margin: 0 15px 0 0;padding-top: 9px'>
+&lt;script async src=&quot;//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js&quot;&gt;&lt;/script&gt;
+&lt;!-- Quickfever MR --&gt;
+&lt;ins class=&quot;adsbygoogle&quot;
+     style=&quot;display:inline-block;width:300px;height:250px&quot;
+     data-ad-client=&quot;ca-pub-8293792128598414&quot;
+     data-ad-slot=&quot;9335513688&quot;&gt;&lt;/ins&gt;
+&lt;script&gt;
+(adsbygoogle = window.adsbygoogle || []).push({});
+&lt;/script&gt;
+</div>
+
+
+<div id='post-body-inner'><div class='post-body-inner'><data:post.body/></div></div>
+
+<script type='text/javascript'>
+function insertAfter(addition,target) {
+var parent = target.parentNode;
+if (parent.lastChild == target) {
+parent.appendChild(addition);
+} else {
+parent.insertBefore(addition,target.nextSibling);
+}
+}
+var adscont = document.getElementById(&quot;quickfever-content&quot;);
+var target = document.getElementById(&quot;post-body-inner&quot;);
+var linebreak = target.getElementsByTagName(&quot;br&quot;);
+if (linebreak.length &gt; 0){
+insertAfter(adscont,linebreak[4]);
+}
+</script>
+
